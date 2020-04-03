a01b0e69e14a37a8a68d97413a4085af813d1726
vikash990 <vikashadubey>
2020-04-03 10:04:39 +0530

    Updates

---
+++ precache-manifest.3935aa1d404e0b6ee0d40819021ef428.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "872fb4bd438ebda7f4502733835eda71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6edb08ba67ae65c86ce6",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "8055b672626569493fe4",
+    "url": "/static/css/main.36c7c39e.chunk.css"
+  },
+  {
+    "revision": "6edb08ba67ae65c86ce6",
+    "url": "/static/js/2.9a21a5ba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9a21a5ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8055b672626569493fe4",
+    "url": "/static/js/main.773afc69.chunk.js"
+  },
+  {
+    "revision": "3fcab70e042d81ffea78",
+    "url": "/static/js/runtime-main.0548e3b8.js"
+  }
+]);
